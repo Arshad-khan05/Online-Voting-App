1e168eab738ea2b84ea361075b92cba878468260
Arshad khan <arshadkhan200205@gmail.com>
2024-08-23 13:10:23 +0530

    Schema defined

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const bodyParser = require('body-parser');
+
+
+app.use(bodyParser.json());
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log('Server is running on port 3000');
+});
