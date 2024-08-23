df33778638ca67956c4593f5dc177049b2ffa84a
Arshad khan <arshadkhan200205@gmail.com>
2024-08-23 23:53:47 +0530

    Database Connected

---
+++ db.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const mongooseUrl = process.env.mongoURLlocal;
+// const mongooseUrl = process.env.mongoURLserver;
+
+mongoose.connect(mongooseUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+
+db.on('connected', () => {
+  console.log('Connected to MongoDB');
+});
+
+
+db.on('disconnected', () => {
+  console.log('DisConnected to MongoDB');
+});
+
+
+module.exports = db;
--- server.js
+++ server.js
@@ -1,17 +1,20 @@
-const express = require('express');
+const express = require('express')
 const app = express();
+const db = require('./db');
 require('dotenv').config();
+
 const bodyParser = require('body-parser');
-
-
 app.use(bodyParser.json());
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
-
-
-
 const PORT = process.env.PORT || 3000;
+
+
+const userRoutes = require('./Routes/userRoutes');
+
+app.use('/user', userRoutes);
+
+
+
+
 app.listen(PORT, () => {
     console.log('Server is running on port 3000');
 });
